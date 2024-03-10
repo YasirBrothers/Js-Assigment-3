6f8b634e7bed8edbfc4bc77690df8c8300a293f8
Muhammad Yasir <154784215+YasirBrothers@users.noreply.github.com>
2024-03-11 02:37:05 +0500

    Update app.js

--- Assignment-3/app.js
+++ Assignment-3/app.js
@@ -1,260 +1,221 @@
-   // Chapter 9-11
+// Chapter# 9-11
 
 
-        //1
+   //Q1
+    // var city = prompt("Enter your city");
 
-        // var City = prompt("Enter City Name");
-        // if(City==="Karachi")
-        // {
-        //     alert("Welcome to the City of Lights");
-        // }
-        // else
-        // {
-        //     alert("Welcome to "+City)
-        // }
+    // if (city == "karachi") {
+    //     console.log("Welcome to The City of Light")
+    // }
 
-        //2
+    //Q2
+        //  var Gender = prompt("Enter Gender","Gender")
 
-        // var Gender = prompt("Enter Gender","Gender")
-
-        // if(Gender == "Male" || Gender == "male" || Gender == "M" || Gender == "m")
-        // {
-        //     alert("Good Morning Sir.")
-        // }
-        // else if(Gender == "Female" || Gender == "female" || Gender == "F" || Gender == "f")
-        // {
-        //     alert("Good Morning Ma'am.")
-        // }
-        // else
-        // {
-        //     alert("Good Morning.")
-        // }
-
-        //3
-
-        // var SignalColour = prompt("Enter Signal Colour","Signal Colour")
-
-        // if(SignalColour == "Red" || SignalColour == "red")
-        // {
-        //     alert("Must Stop")
-        // }
-        // else if(SignalColour == "Yellow" || SignalColour == "yellow")
-        // {
-        //     alert("Ready to Move")
-        // }
-        // else if(SignalColour == "Green" || SignalColour == "green")
-        // {
-        //     alert("Must Move")
-        // }
-        // else
-        // {
-        //     alert("Invalid Signal Colour")
-        // }
+        //  if(Gender == "Male" || Gender == "male" || Gender == "M" || Gender == "m")
+        //  console.log("Good Morning Sir.")
+         
+        //  else if(Gender == "Female" || Gender == "female" || Gender == "F" || Gender == "f")
+        //  console.log("Good Morning Ma'am.")
 
 
-        //4
+    //Q3
+        //   var SignalColour = prompt("Enter Signal Colour","Signal Colour")
 
-        // var currFuel = prompt("Enter Remaining Fuel in your car","In Litres")
+        //  if(SignalColour == "Red" || SignalColour == "red" || SignalColour == "r" || SignalColour == "R")
+        //  console.log("Must Stop!")
 
-        // if(currFuel<0.25)
-        // {
-        //     alert("Please refill the fuel in your car.")
-        // }
-        // else if(currFuel=>0.25)
-        // {
-        //     alert("There is enough fuel to drive.")
-        // }
-        // else
-        // {
-        //     alert("Invalid Entry")
-        // }
+        // else if(SignalColour == "Yellow" || SignalColour == "yellow" || SignalColour == "y" || SignalColour == "Y")
+        //  console.log("Ready to Move!")
 
-        //5
-
-        // var a = 4;
-        // if(++a === 5)
-        // {
-        //     alert("given condition for variable a is true");
-        // }
-        // else
-        // {
-        //     alert("false")
-        // }
-
-        // var b = 82;
-        // if (b++ === 83)
-        // {
-        //     alert("given condition for variable b is true");
-        // }
-        // else
-        // {
-        //     alert("false")
-        // }
-
-        // var c = 12;
-        // if (c++ === 13)
-        // {
-        //     alert("condition 1 is true");
-        // }
-        // if (c === 13)
-        // {
-        //     alert("condition 2 is true");
-        // }
-        // if (++c < 14)
-        // {
-        //     alert("condition 3 is true");
-        // }
-        // if(c === 14)
-        // {
-        //     alert("condition 4 is true");
-        // }
-
-        // var materialCost = 20000;
-        // var laborCost = 2000;
-        // var totalCost = materialCost + laborCost;
-        // if (totalCost === laborCost + materialCost){
-        // alert("The cost equals");
+        // else if(SignalColour == "Green" || SignalColour == "green" || SignalColour == "g" || SignalColour == "G")
+        //  console.log("Move now!")
 
 
-        //6
+    
 
-        // var EngMarks = +prompt("Marks in English?"),
-        // SciMarks = +prompt("Marks in Science?"),
-        // MathMarks = +prompt("Marks in Math?"),
-        // TotalMarks = +prompt("Total Marks"),
-        // Percentage=((EngMarks+SciMarks+MathMarks)/TotalMarks)*100,
-        // Grade,Remarks;
+    //Q4
+        //  var currFuel = prompt("Enter Remaining Fuel in your car","In Litres")
 
-        // if(Percentage >= 80)
-        // {
-        //     Grade="A+";
-        //     Remarks="Excellent";
-        // }
-        // else if(Percentage >= 70)
-        // {
-        //     Grade="A";
-        //     Remarks="Good";
-        // }
-        // else if(Percentage >= 60)
-        // {
-        //     Grade="B";
-        //     Remarks="You need to Improve";
-        // }
-        // else if(Percentage < 60)
-        // {
-        //     Grade="Fail";
-        //     Remarks="Sorry";
-        // }
-        // else
-        // {
-        //     Grade="Invalid";
-        //     Remarks="Invalid";
-        // }
+        //  if(currFuel<0.25)
+        //  {
+        //      console.log("Please refill the fuel in your car.")
+        //  }
+        //  else if(currFuel=>0.25)
+        //  {
+        //      console.log("There is a lots of fuel.")
+        //  }
+         
+        
+    //Q5
+        //  var a = 4;
 
-        //document.write("<h1>Marks Sheet</h1></br></br>Total Marks : "+TotalMarks+"</br>Marks Obtained : "+(EngMarks+SciMarks+MathMarks)+"</br>Percentage : "+Percentage+"%</br>Grade : "+Grade+"</br>Remarks : "+Remarks);
+         
+        //  if(++a === 5)
+        //  {
+        //      alert("given condition for variable a is true");
+        //  }
+        //  else
+        //  {
+        //      alert("false")
+        //  }
 
-        //7
+        //  var b = 82;
+        //  if (b++ === 83)
+        //  {
+        //      alert("given condition for variable b is true");
+        //  }
+        //  else
+        //  {
+        //      alert("false")
+        //  }
 
-        // var Num = 7;
-        // var inputNum = +prompt("Guess a Number Between 1-10")
+        //  var c = 12;
+        //  if (c++ === 13)
+        //  {
+        //      alert("condition 1 is true");
+        //  }
+        //  if (c === 13)
+        //  {
+        //      alert("condition 2 is true");
+        //  }
+        //  if (++c < 14)
+        //  {
+        //      alert("condition 3 is true");
+        //  }
+        //  if(c === 14)
+        //  {
+        //      alert("condition 4 is true");
+        //  }
 
-        // if(Num === inputNum)
-        // {
-        //     alert("Bingo! Correct Answer.")
-        // }
-        // else if (Num === (inputNum+1) || Num === (inputNum-1))
-        // {
-        //     alert("Close enough to the Correct Answer.")
-        // }
-        // else
-        // {
-        //     alert("Incorrect Guess.")
-        // }
+        //  var materialCost = 20000;
+        //  var laborCost = 2000;
+        //  var totalCost = materialCost + laborCost;
+        //  if (totalCost === laborCost + materialCost){
+        //  alert("The cost equals");
+        //  }   
+
+    //Q6
+        //  var EngMarks = +prompt("Marks in English?"),
+        //  SciMarks = +prompt("Marks in Science?"),
+        //  MathMarks = +prompt("Marks in Math?"),
+        //  TotalMarks = +prompt("Total Marks"),
+        //  Percentage=((EngMarks+SciMarks+MathMarks)/TotalMarks)*100,
+        //  Grade,Remarks;
+
+        //  if(Percentage >= 80)
+        //  {
+        //      Grade="A+";
+        //      Remarks="Excellent";
+        //  }
+        //  else if(Percentage >= 70)
+        //  {
+        //      Grade="A";
+        //      Remarks="Good";
+        //  }
+        //  else if(Percentage >= 60)
+        //  {
+        //      Grade="B";
+        //      Remarks="You need to Improve More";
+        //  }
+       
+        //  else if(Percentage >= 50)
+        //  {
+        //      Grade="C";
+        //      Remarks="You need to Improve More";
+        //  }
+       
+        //  else if(Percentage >= 40)
+        //  {
+        //      Grade="D";
+        //      Remarks="You need to Improve More";
+        //  }
+
+        //  else if(Percentage < 40)
+        //  {
+        //      Grade="Fail";
+        //      Remarks="Sorry";
+        //  }
+        //  else
+        //  {
+        //      Grade="Invalid";
+        //      Remarks="Invalid";
+        //  }
+
+        // document.write("<h1>Marks Sheet</h1></br></br>Total Marks : "+TotalMarks+"</br>Marks Obtained : "+(EngMarks+SciMarks+MathMarks)+"</br>Percentage : "+Percentage+"%</br>Grade : "+Grade+"</br>Remarks : "+Remarks);
+
+    //Q7
+        //  var Num = 3;
+        //  var inputNum = +prompt("Guess a Number Between 1 to 10")
+
+        //  if(Num === inputNum)
+        //  {
+        //      alert("Bingo! Correct Answer.")
+        //  }
+        //  else if (Num === (inputNum+1) || Num === (inputNum-1))
+        //  {
+        //      alert("Close enough to the Correct Answer.")
+        //  }
+        //  else
+        //  {
+        //      alert("Incorrect Guess.")
+        //  }
 
 
-        //8
-
-
-        // var InputNum = +prompt("Enter a Number divisible by 3")
-        // if(InputNum%3 === 0)
-        // {
-        //     alert("this number is divisible by 3")
-        // }
-        // else
-        // {
-        //     alert("this number is not divisible by 3")
-        // }
+    //Q8
+        //  var InputNum = +prompt("Enter a Number divisible by 3")
+        //  if(InputNum%3 === 0)
+        //  {
+        //      alert("this number is divisible by 3")
+        //  }
+        //  else
+        //  {
+        //      alert("this number is not divisible by 3")
+        //  }
 
 
         //9
 
-        // var InputNum = +prompt("Enter a Number")
-        // if(InputNum%2 === 0)
-        // {
-        //     alert(InputNum+" is an even number.")
-        // }
-        // else
-        // {
-        //     alert(InputNum+" is an odd number.")
-        // }
+        //  var InputNum = +prompt("Enter a Number")
+        //  if(InputNum%2 === 0)
+        //  {
+        //      alert(InputNum+" is an even number.")
+        //  }
+        //  else
+        //  {
+        //      alert(InputNum+" is an odd number.")
+        //  }
 
 
-        //10
+     //Q10
+        //  var inputTemp=+prompt("Enter Temperature");
 
-        // var inputTemp=+prompt("Enter Tempreture");
+        //  if(inputTemp>=40)
+        //  {
+        //      alert("Its too hot outside.")
+        //  }
+        //  else if(inputTemp>=30)
+        //  {
+        //  alert("The Weather today is Normal."
+        //  }
+        //  else if(inputTemp>=20)
+        //  {
+        //  alert("Today's Weather is Cool.")
+        //  }
+        //  else if(inputTemp>=10)
+        //  {
+        //      alert("OMG! Today's Weather is so Cool.")
+        //  }
+        //  else if(inputTemp < 10)
+        //  {
+        //      alert("So Cold")
+        //  }
+        //  else
+        //  {
+        //      alert("Invalid Temperature")
+        //  }
 
-        // if(inputTemp>=40)
-        // {
-        //     alert("Its too hot outside.")
-        // }
-        // else if(inputTemp>=30)
-        // {
-        //     alert("The Weather today is Normal.")
-        // }
-        // else if(inputTemp>=20)
-        // {
-        //     alert("Today's Weather is Cool.")
-        // }
-        // else if(inputTemp>=10)
-        // {
-        //     alert("OMG! Today's Weather is so Cool.")
-        // }
-        // else if(inputTemp < 10)
-        // {
-        //     alert("Freeeeeez")
-        // }
-        // else
-        // {
-        //     alert("Invalid Tempreture")
-        // }
-
-        //11
-
-        // var Val1 = +prompt("Enter 1st Number");
-        // var Opt = prompt("Enter Operator");
-        // var Val2 = +prompt("Enter 2nd Number");
-        
-        
-        // if(Opt == "+"){
-        //     alert(Val1+Val2)
-        // }
-        // else if(Opt == "-")
-        // {
-        //     alert(Val1-Val2)
-        // }
-        // else if(Opt == "*")
-        // {
-        //     alert(Val1*Val2)
-        // }
-        // else if(Opt == "/")
-        // {
-        //     alert(Val1/Val2)
-        // }
-        // else
-        // {
-        //     alert("Incorrect Operator")
-        // }
-
+    //Q11
+         ???
 
 // Chapter 12-13
 
